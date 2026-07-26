3bd703afc95c73a2120fbb1752ee15b2ba564d92
YewJieh <>
2026-07-26 15:02:02 +0000

    Missing content

---
+++ variables_and_types_in_python/missing_content/main.py
@@ -0,0 +1,12 @@
+# Number of classes and students per class
+classes = 6
+students = 45
+
+# Calculate total number of students
+total_students = 45*6
+# Calculate total number of classestotal_classes_
+total_classes = total_students/27
+
+# Testing
+print("Number of students:", total_students)
+print("Number of groups:", total_classes)
